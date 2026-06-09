0ed4d33e4ef40f8d5f50a100346d999c1a15b81f
Deee_kei <dmitrij.katkov2000@gmail.com>
2026-06-09 10:24:39 +0500

    Исправлена модель пользователя и добавлены отделы

--- apps/accounts/models.py
+++ apps/accounts/models.py
@@ -1,6 +1,18 @@
 from django.contrib.auth.models import AbstractUser
 from django.db import models
 
+class Department(models.Model):
+    name = models.CharField(max_length=255, verbose_name="Название отдела", unique=True)
+    description = models.TextField(blank=True, verbose_name="Описание отдела")
+
+    class Meta:
+        verbose_name = "Отдел"
+        verbose_name_plural = "Отделы"
+
+    def __str__(self):
+        return self.name
+
+
 
 class User(AbstractUser):
     ROLE_CHOICES = (
@@ -35,9 +47,18 @@ class User(AbstractUser):
         related_name="custom_user_permissions"
     )
 
+    departments = models.ManyToManyField(
+        Department,
+        blank=True,
+        related_name='users',
+        verbose_name="Отделы"
+    )
+
     class Meta:
         verbose_name = "Пользователь"
         verbose_name_plural = "Пользователи"
 
     def __str__(self):
         return f"{self.get_full_name()} ({self.get_role_display()})"
+
+
