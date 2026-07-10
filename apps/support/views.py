1b9015d007ea3b3229f0aa2dbb639d4804714584
Deee_kei <dmitrij.katkov2000@gmail.com>
2026-07-10 11:18:01 +0500

    В модуль поддержки добавлены уведомления

--- apps/support/views.py
+++ apps/support/views.py
@@ -2,6 +2,7 @@ from rest_framework import viewsets
 from rest_framework.permissions import IsAuthenticated
 from .models import SupportModule, Ticket
 from .serializers import SupportModuleSerializer, TicketSerializer
+from apps.projects.views import notify_user
 
 class SupportModuleViewSet(viewsets.ModelViewSet):
     queryset = SupportModule.objects.prefetch_related('assignees').all()
@@ -15,4 +16,24 @@ class TicketViewSet(viewsets.ModelViewSet):
 
     def perform_create(self, serializer):
         # При создании заявки автоматически привязываем текущего пользователя как автора
-        serializer.save(author=self.request.user)
+        ticket = serializer.save(author=self.request.user)
+        if ticket.module:
+            # Получаем всех ответственных за этот модуль
+            assignees = ticket.module.assignees.all()
+
+            for assignee in assignees:
+                # Не отправляем уведомление самому себе (если админ сам создал тикет для своего модуля)
+                if assignee != self.request.user:
+                    # Ссылка на страницу заявок на фронтенде
+                    ticket_link = "/tickets"
+                    full_url = f"https://erp.stekufa.ru{ticket_link}"
+
+                    author_name = self.request.user.get_full_name() or self.request.user.username
+
+                    # Отправляем уведомление (Колокольчик + Email + Web Push)
+                    notify_user(
+                        user=assignee,
+                        title="🆕 Новый тикет",
+                        message=f"Поступила новая заявка: {ticket.title}\nМодуль: {ticket.module.name}\nАвтор: {author_name}\nСрочность: {ticket.get_urgency_display()}",
+                        link=ticket_link
+                    )
